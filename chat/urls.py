1386bd2b6a0a27c734aac8cdde43d8cb07b03b70
romzc <ronyramos495@gmail.com>
2024-07-28 16:07:00 -0500

    se agrego la opcion de crear nuevos usuarios y poder ingresar, se añadio la funcioanlidad  de login y logout y que se redireccion los usuarios

--- chat/urls.py
+++ chat/urls.py
@@ -1,9 +1,11 @@
 # chat/urls.py
 from django.urls import path
 from . import views
-
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("<str:room_name>/", views.room, name="room"),
+    path("login/", views.LoginView.as_view(), name="login"),
+    path("logout/", LogoutView.as_view(next_page='login'), name='logout'),
+    path("", views.SearchRoomView.as_view(), name="index"),
+    path("<str:room_name>/", views.RoomView.as_view(), name="room"),
 ]
--- chat/views.py
+++ chat/views.py
@@ -1,9 +1,34 @@
-from django.shortcuts import render
+from django.contrib.auth.forms import AuthenticationForm
+from django.http.response import HttpResponse as HttpResponse
+from django.contrib import messages
+from django.views.generic import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.views import LoginView
+from django.urls import reverse_lazy
 
 # Create your views here.
+class LoginView(LoginView):
+    redirect_authenticated_user = True
+    template_name = "login/login.html"
 
-def index(request):
-    return render(request, "chat/index.html")
+    def  get_success_url(self) -> str:
+        return reverse_lazy('index')
 
-def room(request, room_name):
-    return render(request, "chat/room.html", {"room_name": room_name})
+    def form_invalid(self, form: AuthenticationForm):
+        messages.error(self.request,'Invalid username or password')
+        return self.render_to_response(self.get_context_data(form=form))
+
+
+class SearchRoomView(LoginRequiredMixin, TemplateView):
+    template_name = 'chat/index.html'
+
+
+class RoomView(LoginRequiredMixin, TemplateView):
+
+    template_name = "chat/room.html"
+
+    def get_context_data(self, **kwargs) -> dict[str, any]:
+        context = super().get_context_data(**kwargs)
+        context['room_name'] = self.kwargs['room_name']
+        return context
+
